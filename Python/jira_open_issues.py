a663334dd14d619c49b3ab4e7ba456a6191f3274
Jai Bheemsen Rao <jaibheemsen@gmail.com>
2015-12-14 00:33:11 +0530

    Python Get JIRA

---
+++ Python/jira_open_issues.py
@@ -0,0 +1,10 @@
+import os
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+user_id = os.getenv('JIRA_USER')
+user_password = os.getenv('JIRA_PASS')
+jira_url = os.getenv('JIRA_URL')
+r = requests.get(jira_url + "/rest/api/2/search?jql=assignee=jaibheemsen\\u0040gmail.com%20AND%20status%3DInvestigate", auth=HTTPBasicAuth(user_id, user_password)).json()
+for i in r['issues']:
+        print i['key']
