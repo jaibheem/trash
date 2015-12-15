393212b322da464efb9f85f2ebd4603bac00fce6
Jai Bheemsen Rao <jaibheemsen@gmail.com>
2015-12-15 18:45:47 +0530

    GO | List ENV

---
+++ Go_lang/src/aws-stuff/env.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type org struct {
+	DisplayName  string
+	Environments []string
+}
+
+func jsonGet() ([]byte, error) {
+	user_id := os.Getenv("USERID")
+	user_password := os.Getenv("USERPASSWORD")
+	request, _ := http.NewRequest("GET", "https://api.enterprise.apigee.com/v1/o/jai", nil)
+	request.SetBasicAuth(user_id, user_password)
+	request.Header.Add("Accept", "application/json")
+
+	client := new(http.Client)
+	resp, err := client.Do(request)
+	defer resp.Body.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	var resString []byte
+	resString, err = ioutil.ReadAll(resp.Body)
+	return resString, err
+}
+
+func main() {
+	raw, _ := jsonGet()
+	//	var mapp map[string]interface{}
+	//	var mapp map[interface{}]interface{}
+	var mapp org
+	json.Unmarshal(raw, &mapp)
+	for _, i := range mapp.Environments {
+
+		fmt.Println(i)
+	}
+}
